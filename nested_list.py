3bb1d6684d2004e8ffad7f1b4befe25ddbda5320
Gerardo Macias <gerardo_macias317@ciencias.unam.mx>
2023-07-22 21:24:58 -0600

    Agregando nombre y score a una lista y agregandola dentro de la lista nested_list

--- nested_list.py
+++ nested_list.py
@@ -1,6 +1,11 @@
 if __name__ == '__main__':
+    
+    nested_list = []
+    
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        lista = [name, score]
+        nested_list.append(lista)
         
-        
+    print(nested_list)
