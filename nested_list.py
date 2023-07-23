4258788c094418e89131fadac8273c1e49df6ad5
Gerardo Macias <gerardo_macias317@ciencias.unam.mx>
2023-07-22 22:41:10 -0600

    reformando codigo para puntuaciones negativas

--- nested_list.py
+++ nested_list.py
@@ -10,24 +10,6 @@ if __name__ == '__main__':
         lista = [name, score]
         nested_list.append(lista)
         
-    
-    #to go through every element in this list, use a nested for loop.
-    score = []
-    for inner in nested_list:
-        score.append(inner[1])
-        
-    score.remove(min(score))
-    pos = score.count(score[0])
-    score.sort()
-    
-    
-    names = []
-    for iiner in nested_list:
-        if score[pos-1] in iiner:
-            print(iiner)
-            names.append(iiner)
-            
-    names.sort()
-
-    for i in names:
-        print(i[0])        
+    print(nested_list)    
+    nested_list.sort()
+    print(nested_list)
